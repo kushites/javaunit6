55ba20898eb74a42c751763a89e6e45f4233a0c1
Sanjay Kushwaha <56218670+kushites@users.noreply.github.com>
2022-08-03 21:46:37 +0530

    day 4 Assignment

---
+++ day4/src/main/java/com/day4/Q1/Webapp.java
@@ -0,0 +1,13 @@
+package com.day4.Q1;
+
+public class Webapp {
+
+	public static void main(String[] args) {
+		System.out.println("Ans1:- 1.Web Services can be used to transfer data between Web Applications.\r\n"
+				+ "2.Web Services can be accessed from any languages or platform.\r\n"
+				+ "3.A Web Application is meant for humans to read, while a Web Service is meant for computers to read.\r\n"
+				+ "4.Web Application is a complete Application with a Graphical User Interface (GUI), however, web services do not necessarily have a user interface since it is used as a component in an application.\r\n"
+				+ "5.Web Application can be access through browsers.");
+	}
+
+}
---
+++ day4/src/main/java/com/day4/Q2/Staticdynamic.java
@@ -0,0 +1,10 @@
+package com.day4.Q2;
+
+public class Staticdynamic {
+
+	public static void main(String[] args) {
+		System.out.println("Ans2:- A static website is one with stable content, where every user sees the exact same thing on each individual\n page. On the other hand, a dynamic website is one where content is pulled on-the-fly, allowing its content to \nchange with the user. ");
+
+	}
+
+}
